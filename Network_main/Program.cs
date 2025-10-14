9c02a91c5760d041ab002ec840b3132e4045c443
huseynstudent <huseynstudent5628@gmail.com>
2025-10-14 21:07:38 +0400

    adding networking

--- Network_main/Program.cs
+++ Network_main/Program.cs
@@ -1,27 +1,53 @@
 ﻿using Network_main.Logger;
 using Network_main.User;
-bool letIn = false;
-bool exited= false;
-while (!letIn)
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+internal class Program
 {
-    Console.WriteLine("Welcome to the Network App!\n1. Sign In\n2. Sign Up\n3. Exit");
-    string choice = Console.ReadLine();
-    switch (choice)
+    private static void Main(string[] args)
     {
-        case "1":
-            letIn = Logger.SignIn();
-            break;
-        case "2":
-            Logger.Log.SignUp();
-            letIn = true;
-            break;
-        case "3":
-            exited = true;
-            break;
-        default:
-            Console.WriteLine("Invalid choice, please try again.");
-            break;
+        bool letIn = false;
+        bool exited = false;
+        while (!letIn)
+        {
+            Console.WriteLine("Welcome to the Network App!\n1. Sign In\n2. Sign Up\n3. Exit");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    letIn = Log.SignIn();
+                    break;
+                case "2":
+                    Log.SignUp();
+                    letIn = true;
+                    break;
+                case "3":
+                    exited = true;
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice, please try again.");
+                    break;
+            }
+            if (exited) break;
+        }
+        ////
+        var ip = IPAddress.Parse("127.0.0.1");
+        var receiverport = 45679;
+        var senderport = 45678;
+        ////
+        var server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        var remoteEP = new IPEndPoint(ip, receiverport);
+        server.Bind(remoteEP);
+        server.SendTo(Encoding.UTF8.GetBytes("client connected"), remoteEP);
+        while (!exited)
+        {
+            var msg = Console.ReadLine() ?? string.Empty;
+            if (msg == "" || msg.ToLower() == "exit")
+                exited = true;
+            var data = Encoding.UTF8.GetBytes(msg);
+            server.SendTo(data, remoteEP);
+        }
     }
-    if (exited) break;
 }
-while (!exited); 
